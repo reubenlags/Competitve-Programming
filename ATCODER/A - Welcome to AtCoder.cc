b85a6f758a090beb1e81fb62819c63fe4cecbcd4
Reuben George Jacob <reubengj06@gmail.com>
2026-04-15 10:37:24 +0200

    Add files via upload

---
+++ ATCODER/A - Welcome to AtCoder.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y,z;
+    string s;
+    cin >> x ;
+    cin >> y >> z;
+    cin >> s;
+    cout << x+y+z << " " << s << endl;
+    return 0;
+}
